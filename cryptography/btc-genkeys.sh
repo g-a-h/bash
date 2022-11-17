f6740b752c9ac406cd282a95badaaee0665372c3
gh <117294201+g-a-h@users.noreply.github.com>
2022-11-17 12:02:10 -0700

    Update btc-genkeys.sh

--- cryptography/btc-genkeys.sh
+++ cryptography/btc-genkeys.sh
@@ -1,36 +1,28 @@
-#!/usr/local/env bash 
-# gh - 2022 
-# genkeys bitcoin
+#!/usr/local/env bash
+# gh - 2022
+# genkeybtc
 #######
-hexup() {
+putup() {
   tr "[a-f]" "[A-F]"
 }
 
-pkhex=$(openssl ecparam -genkey -name secp256k1 -noout | openssl ec -text 2>/dev/null \
-| grep -A3 priv | grep -v 'priv' | fmt -120 | sed 's/[: ]//g' |  awk '{printf "%064s\n", $0}' | hexup) 
+pk=$(openssl ecparam -genkey -name secp256k1 -noout)
+pkd=$(openssl ec -text <<<$privatekey 2>/dev/null)
+pkhex=$(openssl ec -text <<<$pkd 2>/dev/null | grep -A3 priv | grep -v 'priv' | fmt -120 | sed -e 's/[: ]//g' -e 's/^00//' | awk '{printf "%064s\n", $0}' | putup) && echo -e "pubkey\t:\t$pkhex" 
+hash=$(openssl ec -pubout -outform DER <<<$pk 2>/dev/null | tail -c 65 | openssl dgst -sha256 -binary | openssl dgst -rmd160 -binary | xxd -p -c 80 | putup) && echo -e "hash\t:\t$hash"
 
-hashpub() { 
-  echo $1 | openssl ec -pubout -outform DER 2>/dev/null | tail -c 65 \
-  | openssl dgst -sha256 -binary | openssl dgst -rmd160 -binary | xxd -p -c 80 | hexup 
-} 
-
-chksum() {
-  echo $1 | xxd -p -r | openssl dgst -sha256 -binary | openssl dgst -sha256 -binary | xxd -p -c 80 | head -c 8 | hexup
-} 
-
-base=($(echo {1..9} {A..Z} {a..z})) 
-encode() { 
-  echo $1 | sed -e 's/\(\(00\)*\).*/\1/' -e 's/00/1/g' | bc <<<"ibase=16; n=$1; while(n>0) { n%3A ; n/=3A }" | tail -r \
-  | while read n 
-      do echo -n ${base[n+1]}
+base=($(echo {1..9} {A..Z} {a..z} | sed 's/[O|I|l]//g' | sed 's/  / /g'))
+encode() {
+  in=$(sed -e 's/\(\(00\)*\).*/\1/' -e 's/00/1/g' <<<$1) && echo -n $in
+  bc <<<"ibase=16; n=$1; while(n>0) { n%3A ; n/=3A }" | tail -r \
+  | while read n
+      do echo -n ${base[n+1]}exit 
     done
 }
 
-hex2addr() { 
-  encode "$2$1$(chksum "$2$1")"
+hex2addr() {
+  echo "$(encode "${2}${1}$(xxd -p -r <<<"$2$1" | openssl dgst -sha256 -binary | openssl dgst -sha256 -binary | xxd -p -c 80 | head -c 8 | putup)")"
 }
-#######
-echo -e "pubkey\t:\t$pkhex"
-hash=$(hashpub $pk) && echo -e "hash\t:\t$hash"
-addr=$(hex2addr $hash "00") && echo -e "address\t:\t$addr" 
+
+addr=$(hex2addr $hash "00") && echo -e "address\t:\t$addr"
 wif=$(hex2addr $pkhex "80") && echo -e "wif\t:\t$wif"
