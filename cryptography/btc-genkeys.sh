d275efc4df87650dcff0fb9c80aaca6478fed31c
gh <117294201+g-a-h@users.noreply.github.com>
2022-11-17 10:10:28 -0700

    Update btc-genkeys.sh

--- cryptography/btc-genkeys.sh
+++ cryptography/btc-genkeys.sh
@@ -6,10 +6,8 @@ hexup() {
   tr "[a-f]" "[A-F]"
 }
 
-gen() { 
-  openssl ecparam -genkey -name secp256k1 -noout | openssl ec -text 2>/dev/null \
-  | grep -A3 priv | grep -v 'priv' | fmt -120 | sed 's/[: ]//g' |  awk '{printf "%064s\n", $0}' | hexup
-} 
+pkhex=$(openssl ecparam -genkey -name secp256k1 -noout | openssl ec -text 2>/dev/null \
+| grep -A3 priv | grep -v 'priv' | fmt -120 | sed 's/[: ]//g' |  awk '{printf "%064s\n", $0}' | hexup) 
 
 hashpub() { 
   echo $1 | openssl ec -pubout -outform DER 2>/dev/null | tail -c 65 \
@@ -23,7 +21,7 @@ chksum() {
 base=($(echo {1..9} {A..Z} {a..z})) 
 encode() { 
   echo $1 | sed -e 's/\(\(00\)*\).*/\1/' -e 's/00/1/g' | bc <<<"ibase=16; n=$1; while(n>0) { n%3A ; n/=3A }" | tail -r \
-  | while read n
+  | while read n 
       do echo -n ${base[n+1]}
     done
 }
@@ -32,7 +30,7 @@ hex2addr() {
   encode "$2$1$(chksum "$2$1")"
 }
 #######
-pkhex=$(gen) && echo -e "pubkey\t:\t$pkhex"
+echo -e "pubkey\t:\t$pkhex"
 hash=$(hashpub $pk) && echo -e "hash\t:\t$hash"
 addr=$(hex2addr $hash "00") && echo -e "address\t:\t$addr" 
 wif=$(hex2addr $pkhex "80") && echo -e "wif\t:\t$wif"
